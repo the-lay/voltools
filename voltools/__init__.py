87cb27717e121d6d6f21854c69dd3122fc00c38f
the-lay <ilja.gubin@gmail.com>
2020-03-21 15:41:06 +0900

    Bumping version to 0.4

--- voltools/__init__.py
+++ voltools/__init__.py
@@ -1,4 +1,4 @@
-__version__ = '0.3.4c'
+__version__ = '0.4.0'
 
 from .transforms import AVAILABLE_INTERPOLATIONS, AVAILABLE_DEVICES, scale, shear, rotate, translate, transform, affine
 from .volume import StaticVolume
