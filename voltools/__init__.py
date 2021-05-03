9425c33217cd62cb25a3af30912e69d6463702e3
the-lay <ilja.gubin@gmail.com>
2021-05-03 21:45:57 +0300

    Bump to 0.4.3

--- voltools/__init__.py
+++ voltools/__init__.py
@@ -1,4 +1,4 @@
-__version__ = '0.4.2'
+__version__ = '0.4.3'
 
 from .transforms import AVAILABLE_INTERPOLATIONS, AVAILABLE_DEVICES, scale, shear, rotate, translate, transform, affine
 from .volume import StaticVolume
