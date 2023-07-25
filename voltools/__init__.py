6073f1c06ee9ec360a16fa7365231a350a2b32e0
Ilja Gubins <ilja.gubin@gmail.com>
2023-07-25 19:32:18 +0200

    Bump to 0.6.0

--- voltools/__init__.py
+++ voltools/__init__.py
@@ -1,4 +1,4 @@
-__version__ = '0.5.0'
+__version__ = '0.6.0'
 
 from .transforms import AVAILABLE_INTERPOLATIONS, AVAILABLE_DEVICES, scale, shear, rotate, translate, transform, affine
 from .volume import StaticVolume
